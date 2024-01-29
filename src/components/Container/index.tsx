b876dbd5f10f93421a88e2c8f223ae249c2a9f03
publishername <62213479+PublisherName@users.noreply.github.com>
2024-01-29 12:36:38 +0545

    Initial Settings

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+interface ContainerProps {
+    children: ReactNode;
+}
+const Container = ({ children,
+}: ContainerProps) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default Container;
